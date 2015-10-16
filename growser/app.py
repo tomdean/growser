7e494978046089d61d7e8c01946f157875e471cb
Tom Dean <tom@tomnipotent.com>
2015-10-16 02:15:14 -0700

    Basic Flask app

---
+++ growser/app.py
@@ -0,0 +1,21 @@
+import logging
+
+from flask import Flask
+
+from growser.services.bigquery import BigQueryService
+
+app = Flask(__name__)
+app.config.from_object('growser.config.BasicConfig')
+app.config.from_envvar('GROWSER_CONFIG', False)
+
+with open(app.config.get('BIGQUERY_PRIVATE_KEY'), 'rb') as fh:
+    key = fh.read()
+
+bigquery = BigQueryService(app.config.get('BIGQUERY_EMAIL'), key, app.config.get('BIGQUERY_PROJECT_ID'))
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s"))
+logging.getLogger().addHandler(consoleHandler)
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
---
+++ growser/config.py
@@ -0,0 +1,11 @@
+
+
+class BasicConfig:
+    GITHUB_OAUTH = ('', '')
+
+    EVENTS_EXPORT_TABLE = "github.events_for_export"
+    EVENTS_EXPORT_PATH = ""
+
+    BIGQUERY_EMAIL = ""
+    BIGQUERY_PRIVATE_KEY = ""
+    BIGQUERY_PROJECT_ID = ""
