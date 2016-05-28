7f09288af3f1a89dbf80755f246cf4e343aae021
Tom Dean <tom@tomnipotent.com>
2016-05-27 23:17:48 -0700

    Migrating services to new system

--- growser/services/celery.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-
-
-def configure(app):
-    celery = Celery('tasks')
-    celery.conf.update(app.config)
-    return celery
--- growser/services/log.py
+++
@@ -1,8 +0,0 @@
-import logging.config
-
-
-def configure(app, cfg=None):
-    app.debug = app.logger.name and app.config.get('DEBUG', False)
-    if cfg:
-        logging.config.fileConfig(cfg)
-    return app.logger
